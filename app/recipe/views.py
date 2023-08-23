cfb0f98e3827dbde826bd450c2b01f637b26084d
Hamdi Özdemir <hamdiozdemir61@gmail.com>
2023-08-23 14:28:59 +0300

    ingredients api added

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -8,7 +8,7 @@ from rest_framework import (
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Recipe, Tag
+from core.models import Recipe, Tag, Ingredient
 from recipe import serializers
 
 
@@ -47,4 +47,19 @@ class TagViewSet(mixins.UpdateModelMixin,
 
     def get_queryset(self):
         """Filter queryset to auth user."""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
+
+
+class IngredientViewSet(mixins.DestroyModelMixin,
+                        mixins.UpdateModelMixin,
+                        mixins.ListModelMixin,
+                        viewsets.GenericViewSet):
+    """Manage ingredients in the databse."""
+    serializer_class = serializers.IngredientSerializer
+    queryset = Ingredient.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """Filter queryset to authenticated user."""
         return self.queryset.filter(user=self.request.user).order_by('-name')
